65f1c2bc12249ea70fb6e856d2e0cfe52bd98f63
Arthurcarre <95696071+Arthurcarre@users.noreply.github.com>
2022-05-18 10:51:25 +0200

    Update Streamlit_App.py

--- Streamlit_App.py
+++ Streamlit_App.py
@@ -9,7 +9,8 @@ data_frame = pd.read_csv('Analyse Pharmacotherapeutique File.csv')
 data_frame.set_index('Index', inplace=True)
 
 option = st.sidebar.selectbox(
-     'Choisis un médicament',
+     "Choisis un médicament. Petite astuce : Il suffit de cliquer sur la barre de recherche, pas besoin d'effacer, et de taper les première lettres du"
+     " médicament (DCI ou Princeps).",
      set(data_frame.index))
 
 st.sidebar.write('Tu as sélectionné:', option)
