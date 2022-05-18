464c12589dd5b5b7c5545ed77eaa7ddd77280f15
Arthurcarre <95696071+Arthurcarre@users.noreply.github.com>
2022-05-18 21:12:27 +0200

    Update Streamlit_App.py

--- Streamlit_App.py
+++ Streamlit_App.py
@@ -3,7 +3,6 @@ import pandas as pd
 
 logo = st.sidebar.image('img/logo_chm.png')
 st.sidebar.caption("Guide d'analyse pharmacothérapeutique chez le patient MUPA.")
-st.subheader('À adapter selon le contexte')
 
 data_frame = pd.read_csv('Analyse Pharmacotherapeutique File.csv')
 data_frame.set_index('Index', inplace=True)
@@ -22,6 +21,7 @@ col1, col2 = st.columns(2)
 if st.sidebar.button("Ajouter le médicament"):
      col1.write(option)
      with col2 :
+          st.subheader('À adapter selon le contexte')
           with st.expander("Commentaires"):                                  
                compteur = 0
                for i in data_frame.index: 
