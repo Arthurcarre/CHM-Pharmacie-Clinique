706d1d526d3ddf124c3763d64ec34acb4646890b
Arthurcarre <95696071+Arthurcarre@users.noreply.github.com>
2022-05-21 08:28:40 +0200

    Update Streamlit_App.py

--- Streamlit_App.py
+++ Streamlit_App.py
@@ -15,7 +15,7 @@ col1.subheader('Liste des prescriptions')
 col2.subheader('Commentaires à adapter')
 
 option = st.sidebar.selectbox(
-     "Choisis un médicament. Petite astuce : Il suffit de cliquer sur la barre de recherche, pas besoin d'effacer, et de taper les première lettres du"
+     "Choisis un médicament. Petite astuce : Il suffit de cliquer sur la barre de recherche (pas besoin d'effacer) et de taper les première lettres du"
      " médicament (DCI ou Princeps).",
      liste_medoc)
 
@@ -45,4 +45,40 @@ if "liste_presc" in st.session_state:
                             compteur += 1
 
                     for i in range(compteur):
-                        txt = st.text_area(f"{data_frame.loc[{medoc}, 'Condition'][i]}", f"{data_frame.loc[{medoc}, 'Paragraphe'][i]}", max_chars=500)                   
+                        txt = st.text_area(f"{data_frame.loc[{medoc}, 'Condition'][i]}", f"{data_frame.loc[{medoc}, 'Paragraphe'][i]}", max_chars=500) 
+
+torsadogene_num = []
+torsadogene_name = []
+hypok_num = []
+hypok_name = []
+hyperk_num = []
+hyperk_name = []
+                    
+if "liste_presc" in st.session_state:
+     for medoc in st.session_state.liste_presc :
+          if data_frame.loc[{medoc}, 'Torsadogène'][0] == 1 :
+               torsadogene_num.append(int(data_frame.loc[{medoc}, 'Torsadogène'][0]))
+               torsadogene_name.append(medoc)
+          if data_frame.loc[{medoc}, 'Torsadogène'][0] == 1 :
+               hypok_num.append(int(data_frame.loc[{medoc}, 'Hypok'][0]))
+               hypok_name.append(medoc)
+          if data_frame.loc[{medoc}, 'Torsadogène'][0] == 1 :
+               hyperk_num.append(int(data_frame.loc[{medoc}, 'HyperK'][0]))
+               hyperk_name.append(medoc)
+     col1, col2 = st.columns(2)
+     col1.write(f"Nombre de médicament torsadogène dans cette prescription : {torsadogene_num.sum()}")
+     with col2 :
+          with st.expander("Médicament(s) torsadogène(s) de la prescription"):
+               for name in torsadogene_name : 
+                    st.write(f"\n {name}")
+     col1.write(f"Nombre de médicament hypokaliémiant dans cette prescription : {hypok_num.sum()}")
+     with col2 :
+          with st.expander("Médicament(s) hypokaliémiant(s) de la prescription"):
+               for name in hypok_name : 
+                    st.write(f"\n {name}")
+     col1.write(f"Nombre de médicament torsadogène dans cette prescription : {hyperk_num.sum()}")
+     with col2 :
+          with st.expander("Médicament(s) hyperkaliémiant(s) de la prescription"):
+               for name in hyperk_name : 
+                    st.write(f"\n {name}")
+     
