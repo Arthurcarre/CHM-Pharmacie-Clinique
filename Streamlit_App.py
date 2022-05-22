3848d009ddd347606c7de1f9b346ce58347cff01
Arthurcarre <95696071+Arthurcarre@users.noreply.github.com>
2022-05-22 21:24:28 +0200

    Update Streamlit_App.py

--- Streamlit_App.py
+++ Streamlit_App.py
@@ -103,58 +103,67 @@ if "liste_presc" in st.session_state:
                pro_convuls_num.append(int(data_frame.loc[{medoc}, 'Pro_convul'][0]))
                pro_convuls_name.append(medoc)
      
-     col1, col2 = st.columns(2)
-     col1.write(f"Charge anticholinergique périphérique de cette prescription : {np.sum(anticho_periph_num)}")
-     with col2 :
-          with st.expander("Médicament(s) anticholinergique(s) de la prescription"):
-               for name in anticho_periph_name : 
-                    st.write(f"\n {name}")
-     col1, col2 = st.columns(2)
-     col1.write(f"Charge anticholinergique centrale de cette prescription : {np.sum(anticho_central_num)}")
-     with col2 :
-          with st.expander("Médicament(s) anticholinergique(s) avec action central dans cette prescription"):
-               for name in anticho_central_name : 
-                    st.write(f"\n {name}")
-     col1, col2 = st.columns(2)
-     col1.write(f"Nombre de médicament torsadogène dans cette prescription : {np.sum(torsadogene_num)}")
-     with col2 :
-          with st.expander("Médicament(s) torsadogène(s) de la prescription"):
-               for name in torsadogene_name : 
-                    st.write(f"\n {name}")
-     col1, col2 = st.columns(2)
-     col1.write(f"Nombre de médicament hypokaliémiant dans cette prescription : {np.sum(hypok_num)}")
-     with col2 :
-          with st.expander("Médicament(s) hypokaliémiant(s) de la prescription"):
-               for name in hypok_name : 
-                    st.write(f"\n {name}")
-     col1, col2 = st.columns(2)
-     col1.write(f"Nombre de médicament hyperkaliémiant dans cette prescription : {np.sum(hyperk_num)}")
-     with col2 :
-          with st.expander("Médicament(s) hyperkaliémiant(s) de la prescription"):
-               for name in hyperk_name : 
-                    st.write(f"\n {name}")                
-     col1, col2 = st.columns(2)
-     col1.write(f"Nombre de médicament dépresseur du système nerveux central dans cette prescription : {np.sum(depresseur_SNC_num)}")
-     with col2 :
-          with st.expander("Médicament(s) dépresseur(s) du système nerveux central de la prescription"):
-               for name in depresseur_SNC_name : 
-                    st.write(f"\n {name}")
-     col1, col2 = st.columns(2)
-     col1.write(f"Nombre de médicament potentiellement inapproprié chez la personne âgée dans cette prescription : {np.sum(medoc_inaprop_num)}")
-     with col2 :
-          with st.expander("Médicament(s) potentiellement inapproprié(s) chez la personne âgée de la prescription"):
-               for name in medoc_inaprop_name : 
-                    st.write(f"\n {name}")
-     col1, col2 = st.columns(2)
-     col1.write(f"Nombre de médicament à l'origine d'hypotension (hors antihypertenseur), notamment orthostatique, dans cette prescription : {np.sum(hypoT_ortho_num)}")
-     with col2 :
-          with st.expander("Médicament(s) à l'origine d'hypotension (hors antihypertenseur), notamment orthostatique, de la prescription"):
-               for name in hypoT_ortho_name : 
-                    st.write(f"\n {name}")
-     col1, col2 = st.columns(2)
-     col1.write(f"Nombre de médicament proconvulsivant dans cette prescription : {np.sum(pro_convuls_num)}")
-     with col2 :
-          with st.expander("Médicament(s) proconvulsivant(s) de la prescription"):
-               for name in pro_convuls_name : 
-                    st.write(f"\n {name}")
+     if np.sum(anticho_periph_num) > 0 :
+          col1, col2 = st.columns(2)
+          col1.write(f"Charge anticholinergique périphérique de cette prescription : {np.sum(anticho_periph_num)}")
+          with col2 :
+               with st.expander("Médicament(s) anticholinergique(s) de la prescription"):
+                    for name in anticho_periph_name : 
+                         st.write(f"\n {name}")
+     if np.sum(anticho_central_num) > 0 :
+          col1, col2 = st.columns(2)
+          col1.write(f"Charge anticholinergique centrale de cette prescription : {np.sum(anticho_central_num)}")
+          with col2 :
+               with st.expander("Médicament(s) anticholinergique(s) avec action central dans cette prescription"):
+                    for name in anticho_central_name : 
+                         st.write(f"\n {name}")
+     if np.sum(torsadogene_num) > 0 :
+          col1, col2 = st.columns(2)
+          col1.write(f"Nombre de médicament torsadogène dans cette prescription : {np.sum(torsadogene_num)}")
+          with col2 :
+               with st.expander("Médicament(s) torsadogène(s) de la prescription"):
+                    for name in torsadogene_name : 
+                         st.write(f"\n {name}")
+     if np.sum(hypok_num) > 0 :
+          col1, col2 = st.columns(2)
+          col1.write(f"Nombre de médicament hypokaliémiant dans cette prescription : {np.sum(hypok_num)}")
+          with col2 :
+               with st.expander("Médicament(s) hypokaliémiant(s) de la prescription"):
+                    for name in hypok_name : 
+                         st.write(f"\n {name}")
+     if np.sum(hyperk_num) > 0 :
+          col1, col2 = st.columns(2)
+          col1.write(f"Nombre de médicament hyperkaliémiant dans cette prescription : {np.sum(hyperk_num)}")
+          with col2 :
+               with st.expander("Médicament(s) hyperkaliémiant(s) de la prescription"):
+                    for name in hyperk_name : 
+                         st.write(f"\n {name}")                
+     if np.sum(depresseur_SNC_num) > 0 :
+          col1, col2 = st.columns(2)
+          col1.write(f"Nombre de médicament dépresseur du système nerveux central dans cette prescription : {np.sum(depresseur_SNC_num)}")
+          with col2 :
+               with st.expander("Médicament(s) dépresseur(s) du système nerveux central de la prescription"):
+                    for name in depresseur_SNC_name : 
+                         st.write(f"\n {name}")
+     if np.sum(medoc_inaprop_num) > 0 :
+          col1, col2 = st.columns(2)
+          col1.write(f"Nombre de médicament potentiellement inapproprié chez la personne âgée dans cette prescription : {np.sum(medoc_inaprop_num)}")
+          with col2 :
+               with st.expander("Médicament(s) potentiellement inapproprié(s) chez la personne âgée de la prescription"):
+                    for name in medoc_inaprop_name : 
+                         st.write(f"\n {name}")
+     if np.sum(hypoT_ortho_num) > 0 :
+          col1, col2 = st.columns(2)
+          col1.write(f"Nombre de médicament à l'origine d'hypotension (hors antihypertenseur), notamment orthostatique, dans cette prescription : {np.sum(hypoT_ortho_num)}")
+          with col2 :
+               with st.expander("Médicament(s) à l'origine d'hypotension (hors antihypertenseur), notamment orthostatique, de la prescription"):
+                    for name in hypoT_ortho_name : 
+                         st.write(f"\n {name}")
+     if np.sum(pro_convuls_num) > 0 :
+          col1, col2 = st.columns(2)
+          col1.write(f"Nombre de médicament proconvulsivant dans cette prescription : {np.sum(pro_convuls_num)}")
+          with col2 :
+               with st.expander("Médicament(s) proconvulsivant(s) de la prescription"):
+                    for name in pro_convuls_name : 
+                         st.write(f"\n {name}")
      
