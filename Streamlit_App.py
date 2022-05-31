3c8f2aed3e3e0badea4baa436478d6219894d9a5
Arthurcarre <95696071+Arthurcarre@users.noreply.github.com>
2022-05-31 20:06:00 +0200

    Update Streamlit_App.py

--- Streamlit_App.py
+++ Streamlit_App.py
@@ -192,7 +192,7 @@ option2 = st.sidebar.selectbox(
      "Choisis une liste pour la consulter.",
      Listes_medocs.index)
 
-if isinstance((Listes_medocs.loc[{option2}, 'Listes']), float) == False :
+if isinstance((Listes_medocs.loc[{option2}, 'Listes'][0]), float) == False :
      st.text_area(option2, 
                   f"{Listes_medocs.loc[{str(option2)}, 'Listes'][0]}",
                   key = option2)
