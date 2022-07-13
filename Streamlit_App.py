d9709d225174723b2339f52b446b344f5c03219e
Arthurcarre <95696071+Arthurcarre@users.noreply.github.com>
2022-07-13 20:47:25 +0200

    Update Streamlit_App.py

--- Streamlit_App.py
+++ Streamlit_App.py
@@ -4,6 +4,9 @@ import numpy as np
 from bokeh.models.widgets import Button
 from bokeh.models import CustomJS
 from streamlit_bokeh_events import streamlit_bokeh_events
+from streamlit.logger import get_logger
+
+LOGGER = get_logger(__name__)
 
 def run():
     st.set_page_config(
