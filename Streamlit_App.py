d6af690eb660b16cc62ec7af9d1a82acfc6d3ab8
Arthurcarre <95696071+Arthurcarre@users.noreply.github.com>
2022-08-15 17:55:39 +0200

    Update Streamlit_App.py

--- Streamlit_App.py
+++ Streamlit_App.py
@@ -140,53 +140,33 @@ def main():
                                                         override_height=40,
                                                         debounce_time=0)
                                      else :
-                                        if data_frame.loc[{medoc}, 'Category'][i] == '0' :
-                                           st.text_area(f"{data_frame.loc[{medoc}, 'Condition'][i]}",
-                                                   f"{data_frame.loc[{medoc}, 'Paragraphe'][i]}",
-                                                   key = medoc, max_chars=500, help="Source : À complèter")
-                                           text_to_be_copied = data_frame.loc[{medoc}, 'Paragraphe'][i]
-                                           copy_dict = {"content": text_to_be_copied}
+                                        #try :
+                                        if data_frame.loc[{medoc}, 'Category'][i] != data_frame.loc[{medoc}, 'Category'][i-1] :
+                                             st.write(" ----------------------------- ") 
+                                             st.write(f"**{data_frame.loc[{medoc}, 'Category'][i]}**")
+                                        txt = st.checkbox(f"{data_frame.loc[{medoc}, 'Condition'][i]}",
+                                                         key = medoc)
+                                        #except :
+                                        #st.error("ERROR : La base de données interne contient un duplicat (Nom de médicament + Condition). À corriger")
+                                        if txt :                
+                                            st.text_area("À adapter selon le contexte", 
+                                                        f"{data_frame.loc[{medoc}, 'Paragraphe'][i]}",
+                                                        key = int(np.random.randint(0, 100000, size=(1, 1))), max_chars=500, help="Source : À complèter")
+                                            text_to_be_copied = data_frame.loc[{medoc}, 'Paragraphe'][i]
+                                            copy_dict = {"content": text_to_be_copied}
 
-                                           copy_button = Button(label="Copier le texte")
-                                           copy_button.js_on_event("button_click", CustomJS(args=copy_dict, code="""
-                                               navigator.clipboard.writeText(content);
-                                               """))
+                                            copy_button = Button(label="Copier le texte")
+                                            copy_button.js_on_event("button_click", CustomJS(args=copy_dict, code="""
+                                                navigator.clipboard.writeText(content);
+                                                """))
 
-                                           no_event = streamlit_bokeh_events(
-                                               copy_button,
-                                               events="GET_TEXT",
-                                               key=int(np.random.randint(0, 100000, size=(1, 1))),
-                                               refresh_on_update=True,
-                                               override_height=40,
-                                               debounce_time=0)
-                                        else :
-                                            #try :
-                                            if data_frame.loc[{medoc}, 'Category'][i] != data_frame.loc[{medoc}, 'Category'][i-1] :
-                                                 st.write(" ----------------------------- ") 
-                                                 st.write(f"**{data_frame.loc[{medoc}, 'Category'][i]}**")
-                                            txt = st.checkbox(f"{data_frame.loc[{medoc}, 'Condition'][i]}",
-                                                             key = int(np.random.randint(0, 100000, size=(1, 1))))
-                                            #except :
-                                            #st.error("ERROR : La base de données interne contient un duplicat (Nom de médicament + Condition). À corriger")
-                                            if txt :                
-                                                st.text_area("À adapter selon le contexte", 
-                                                            f"{data_frame.loc[{medoc}, 'Paragraphe'][i]}",
-                                                            key = int(np.random.randint(0, 100000, size=(1, 1))), max_chars=500, help="Source : À complèter")
-                                                text_to_be_copied = data_frame.loc[{medoc}, 'Paragraphe'][i]
-                                                copy_dict = {"content": text_to_be_copied}
-
-                                                copy_button = Button(label="Copier le texte")
-                                                copy_button.js_on_event("button_click", CustomJS(args=copy_dict, code="""
-                                                    navigator.clipboard.writeText(content);
-                                                    """))
-
-                                                no_event = streamlit_bokeh_events(
-                                                    copy_button,
-                                                    events="GET_TEXT",
-                                                    key=int(np.random.randint(0, 100000, size=(1, 1))),
-                                                    refresh_on_update=True,
-                                                    override_height=40,
-                                                    debounce_time=0)  
+                                            no_event = streamlit_bokeh_events(
+                                                copy_button,
+                                                events="GET_TEXT",
+                                                key=int(np.random.randint(0, 100000, size=(1, 1))),
+                                                refresh_on_update=True,
+                                                override_height=40,
+                                                debounce_time=0)  
 
                       if data_frame.loc[{medoc}, 'Inducteur_enz'][0] == 1 :
                            col1.warning("Inducteur enzymatique puissant")
